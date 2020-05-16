48270c05a82d8784bc86289810b3fac66dc43854
brunyyta <bruna.dos.santosb@gmail.com>
2020-05-17 09:01:53 +1000

    Until lesson 6  - Introduction

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,23 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template:`
-  <p>
-    input-button-unit works!
-    The title is {{title}}
-  
-  </p>`,
-  //templateUrl: './input-button-unit.component.html',
+  templateUrl: './input-button-unit.component.html',
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
-
-  constructor() { }
-
   title = "Hello World";
 
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
